9932812b554d1ac175e10e8c8eb78b6be60ecf10
Your Name <116570267+Orikeysar@users.noreply.github.com>
2024-07-26 16:00:11 +0300

    Update carService.js

--- src/services/carService.js
+++ src/services/carService.js
@@ -1,8 +1,8 @@
-import { collection, addDoc, getDocs, doc, deleteDoc, updateDoc } from 'firebase/firestore';
+import { collection, addDoc, getDocs, doc, deleteDoc, updateDoc,getDoc } from 'firebase/firestore';
 import { db } from '../firebase.config';
-
+import { getStorage, ref, deleteObject } from "firebase/storage";
 const carCollection = collection(db, 'cars');
-
+const storage = getStorage();
 
 
 export const getCars = async () => {
@@ -19,8 +19,34 @@ export const addCar = async (carData) => {
   return { id: docRef.id, ...carData };
 };
 
+const deleteImagesFromStorage = async (imageUrls) => {
+  try {
+    const deletePromises = imageUrls.map((url) => {
+      const imageRef = ref(storage, url);
+      return deleteObject(imageRef);
+    });
+    await Promise.all(deletePromises);
+  } catch (error) {
+    console.error('Error deleting images from storage:', error);
+  }
+};
+
 export const deleteCar = async (id) => {
-  await deleteDoc(doc(db, 'cars', id));
+  try {
+    const carDoc = doc(db, 'cars', id);
+    const carSnapshot = await getDoc(carDoc);
+    if (carSnapshot.exists()) {
+      const carData = carSnapshot.data();
+      await deleteImagesFromStorage(carData.carImages);
+      await deleteDoc(carDoc);
+      console.log('Car deleted successfully');
+    } else {
+      console.error('No such document!');
+    }
+  } catch (error) {
+    console.error('Error deleting car:', error);
+    throw new Error('Failed to delete car');
+  }
 };
 
 export const updateCar = async (id, updatedData) => {
